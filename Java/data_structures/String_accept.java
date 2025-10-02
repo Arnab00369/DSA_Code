5f902e48628f79b1d68cc379f55ce71d3f9828e3
Arnab Jana <arnab003jn.369@gmail.com>
2025-10-02 20:50:42 +0530

    Create String_accept.java

---
+++ Java/data_structures/String_accept.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a String: ");
+    String s = sc.nextLine();
+    System.out.println("The String is: "+s+" added with a new token of String");    
+  }
+}
